e32da937311eb0f414856dea008913e9a787d873
Taaya Rai <124600727+taayaGitHub1@users.noreply.github.com>
2026-04-22 22:07:23 +0545

    added basic logic to answeranlyzer service

--- QuestionScrapper/Services/AnswerAnalyzerService.cs
+++ QuestionScrapper/Services/AnswerAnalyzerService.cs
@@ -1,4 +1,5 @@
 using QuestionScrapper.Models;
+using System.Runtime.InteropServices.Java;
 
 namespace QuestionScrapper.Services
 {
@@ -7,9 +8,27 @@ namespace QuestionScrapper.Services
 
         public int EvaluateAnswers(List<Question> questions, List<StudentAnswer> answers)
         {
-                        return 0;
+            var summark<int>= new summark<int> = 0;
+            for (int i = 0; i < questions.Count; i++) { }
+            var questionembed = GetEmbedding(questions[i]);
+            var answersembed = GetEmbedding(answers[i]);
+            similarity = cosine(questionembed, answersembed);
+            if similarity > 0.75{
+                obtainedmark = 5;
+            }
+            else if similarity > .5{
+                obtainedmark = 3;
+            }
+            else
+            {
+                obtainedmark = 0;
+            }
+            summark = sumark.Add(obtainedemark);
+            return 0;
         }
-
-
+        public int GetEmbedding(Question embed) { }
+        public int cosine(int a, int b) { }
     }
+
+
 }
